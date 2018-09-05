258c3dca46fb9860a9c6cd97c24b5b04aedd364d
Kidd <HEDE0001@E.NTU.EDU.SG>
2018-09-05 22:17:28 +0800

    Add files via upload

--- Code.py
+++ Code.py
@@ -22,30 +22,40 @@ from statsmodels import robust
 from WindPy import w
 w.start()
 
-#%% 1.1 数据获取
-"""
-主体数据来源于万得，补充数据用 python WindPy 包下载
-股票池为全A股，因子列表参见附录
-"""
+#%% 参数设置
 
-# 定义函数返回月末日期
-def get_month_end(any_day):
+# 1. 全行业代码表
+ind_code_df = pd.read_csv('ind_code_dict.csv',dtype=str, index_col=0)
+ind_code_dict =ind_code_df.to_dict()['industry_code']
+
+# 2. 沪深 300 指数收益表
+hs300_df = pd.read_csv('hs300.csv', index_col=0)
+hs300_dict =hs300_df.to_dict()['hs300_return_t_plus']
+
+# 3. 截面日期字典
+def get_month_end(any_day):    # 定义函数：返回月末日期
     month_next = any_day.replace(day=28) + dt.timedelta(days=4)    # 跳到下一月
     return month_next - dt.timedelta(days=month_next.day)    #  倒回上月最后一日
 
-# 获取全截面字典（1998-2017年每月月末为一截面）
 section_dict = {}
 section_num = -2
-
-for year in range (1998,2017+1):
+for year in range (1998,2017+1):    # 获取全截面日期字典
     for month in range(1, 12+1):
         month_end = str(get_month_end(dt.date(year,month,1)))
         section_dict[section_num]=month_end
         section_num = section_num + 1
 
+# 4. 模型参数
+select_percent = [0.3, 0.3]    # 标记样本比例
+
+#%% 1.1 数据获取
+"""
+主体数据来源于万得，补充数据用 python WindPy 包下载
+股票池为全A股，因子列表参见附录
+
 # 从万得下载因子数据
 section_range = list(range(230, 237+1))    # 设置样本空间边界
-"""
+
 for section_num in section_range:   # 下载数据
     list_stock = w.wset("SectorConstituent","date=%s" % section_dict[section_num],"sector=全部A股" ).Data[1]
     section = w.wss(list_stock, "ipo_date,sec_status,close", "tradeDate=%s" % section_dict[section_num])    # 设置要下载的因子
@@ -68,7 +78,7 @@ for section_num in section_range:   # 下载数据
 3) 剔除市值取对数和股价取对数缺省的样本
 """
 
-section_num_list = range(82, 208+1)    # 设定取样范围
+section_num_list = range(82, 82+1)    # 设定取样范围
 
 for section_num in section_num_list:
     data_0 = pd.read_csv('data_0/%s.csv' % section_num, index_col=0)
@@ -94,40 +104,7 @@ for section_num in section_num_list:
     # 输出文件
     # data_2.to_csv('data_2/%s_2.csv' % section_num)
 
-
-#%% 2.1 标签提取
-"""
-1) 以下一个自然月的个股超额收益作为样本的标签（以沪深300指数为基准）
-2) 剔除截面本月或下一个月停牌的股票：通过下一月收益是否空值判断
-"""
-
-# 定义函数：计算本月收益及下月收益
-section_num_list = range(83, 207+1)    # 设定取样范围
-
-for section_num in section_num_list:
-    # 读取上月、本月、下月数据
-    df_t_minus = pd.read_csv('data_2/%s_2.csv' % (section_num-1), index_col=0, usecols=[0,55])
-    df_t       = pd.read_csv('data_2/%s_2.csv' % (section_num),   index_col=0, usecols=[0,55])
-    df_t_plus  = pd.read_csv('data_2/%s_2.csv' % (section_num+1), index_col=0, usecols=[0,55])
-    df_t_all   = pd.read_csv('data_2/%s_2.csv' % (section_num), index_col=0)
-
-    # 合并数据框
-    df_return = pd.concat([df_t_minus, df_t, df_t_plus], axis=1, join_axes=[df_t.index])
-    df_return.columns = ['CLOSE_t_minus', 'CLOSE_t', 'CLOSE_t_plus']
-
-    # 计算收益率
-    df_return['return_t'] = np.log(df_return['CLOSE_t'] / df_return['CLOSE_t_minus'])
-    df_return['return_t_plus'] = np.log(df_return['CLOSE_t_plus'] / df_return['CLOSE_t'])
-    data_3 = pd.concat([df_t_all, df_return], axis=1, join_axes=[df_t_all.index])
-
-    # 剔除停牌样本和中间变量
-    data_3 = data_3.dropna()
-    data_3 = data_3.drop(['CLOSE', 'CLOSE_t_minus', 'CLOSE_t', 'CLOSE_t_plus'], axis=1)
-
-    # 输出文件
-    # data_3.to_csv('data_3/%s_3.csv' % section_num)
-
-#%% 2.2 特征预处理
+#%% 3.1 特征预处理
 """
 1) 缺失值: 因数据获取困难，不按照原文做法（设为行业均值），改为直接删除因子暴露缺失的样本
 2) 去极值: MdAD 法(Median Absolute Deviation), 参数沿用研报用的 5
@@ -144,7 +121,101 @@ def filter_EV_MdAD(series, n):
     return np.clip(series,min_range,max_range)
 
 
-# 定义函数： 依据万得行业分类，标记行业哑变量：
+# 定义函数：标记行业哑变量：
+def industry_dummy(df):
+
+    # 创建当截面的行业代码字典
+    ind_dict = {}
+    for stk in list(df.index):
+        ind_dict['%s' % stk] = ind_code_dict['%s' % stk]
+    # 标记行业哑变量
+    identity_array = np.identity(len(ind_dict))
+    df = pd.DataFrame(identity_array, index=ind_dict.keys(), columns=ind_dict.values())
+    df = df.groupby(df.columns, axis=1).sum()
+
+    return df
+
+
+# 特征预处理
+section_num_list = range(82, 208+1)    # 设定取样范围
+
+for section_num in section_num_list:
+    data_2 = pd.read_csv('data_2/%s_2.csv' % section_num, index_col=0, usecols=range(54+1))
+    data_3 = pd.read_csv('data_2/%s_2.csv' % section_num, index_col=0)
+    data_2['ln_capital_4ols'] = data_2['ln_capital']
+    factor_list = list(data_2.columns)
+    factor_list.remove('DP')    # 此因子0值过多，不适宜用中位数法去极值，故排除
+    factor_list.remove('ln_capital_4ols')
+
+    # 去极值
+    for column in factor_list:
+        data_2[column] = filter_EV_MdAD(data_2[column], 5)
+
+    # 中性化和序列化
+    factor_list.append('DP')
+    data_2_ind = industry_dummy(data_2)
+    data_2_ind_LnMtkCap = pd.concat([data_2['ln_capital_4ols'], data_2_ind], axis=1, join_axes=[data_2.index])
+
+    for column in factor_list:
+        # 中性化：
+        neu_model = sm.OLS(data_2[column], data_2_ind_LnMtkCap).fit()
+        data_2[column] = neu_model.resid
+        # 序列化：
+        data_2_1 = data_2.sort_values(by=column)
+        data_2_1[column] = list(range(1, len(data_2)+1))
+        data_3[column] = data_2_1[column] / len(data_2)
+
+    # 输出文件
+    data_3.to_csv('data_3/%s_3.csv' % section_num)
+
+
+#%% 3.2 标签提取和标记
+"""
+1) 以下一个自然月的个股超额收益作为样本的标签（以沪深300指数为基准），正例为1，负例为0
+2) 剔除截面本月或下一个月停牌的股票：通过下一月收益是否空值判断
+"""
+
+# 定义函数：将超额收益高的标记为 1，超额收益低的标记为 0
+def label(df):
+    df['return_bin'] = np.nan
+    df = df.sort_values(by='return_t_plus', ascending=False)    # 排序
+    n_stk_select = np.multiply(select_percent, len(df))    # 设置切片数
+    n_stk_select = np.around(n_stk_select).astype(int)    # 取整
+    # 标记正负例
+    df.iloc[0:n_stk_select[0], -1] = 1
+    df.iloc[-n_stk_select[1]:, -1] = 0
+    df = df.dropna(axis=0)
+    return df
+
+section_num_list = range(82, 207+1)    # 设定取样范围
+
+for section_num in section_num_list:
+    # 读取本月、下月数据
+    df_t       = pd.read_csv('data_3/%s_3.csv' % (section_num),   index_col=0, usecols=[0,55])
+    df_t_plus  = pd.read_csv('data_3/%s_3.csv' % (section_num+1), index_col=0, usecols=[0,55])
+    df_t_all   = pd.read_csv('data_3/%s_3.csv' % (section_num), index_col=0, usecols=range(54+1))
+
+    # 合并数据框
+    df_return = pd.concat([df_t, df_t_plus], axis=1, join_axes=[df_t.index])
+    df_return.columns = ['CLOSE_t', 'CLOSE_t_plus']
+
+    # 计算超额收益率
+    hs300_return = hs300_dict[section_dict[section_num]]    # 获取当期的沪深300下月收益率
+    df_return['return_t_plus'] = (np.log(df_return['CLOSE_t_plus'] / df_return['CLOSE_t'])) - hs300_return
+    data_4 = pd.concat([df_t_all, df_return], axis=1, join_axes=[df_t_all.index])
+
+    # 标记正负例
+    data_4 = label(data_4)
+    data_4 = data_4.drop(['CLOSE_t', 'CLOSE_t_plus', 'return_t_plus'], axis=1)    # 删除中间变量
+
+    # 输出文件
+    data_4.to_csv('data_4/%s_4.csv' % section_num)
+
+#%% 备用代码
+
+"""
+# 从万得获取行业字典
+
 def wind_ind_dict(df, date):
     # 从万得获取行业字典
     stock_list = df.index
@@ -159,46 +230,10 @@ def wind_ind_dict(df, date):
     return df_ind
 
 
-# 特征预处理
-section_num_list = range(85, 85+1)    # 设定取样范围
-
-for section_num in section_num_list:
-    data_3 = pd.read_csv('data_3/%s_3.csv' % section_num, index_col=0)
-    data_4 = pd.read_csv('data_3/%s_3.csv' % section_num, index_col=0)
-    data_3['ln_capital_4ols'] = data_3['ln_capital']
-    factor_list = list(data_3.columns)
-    factor_list.remove('DP')
-    factor_list.remove('ln_capital_4ols')
-    factor_list.remove('return_t')
-    factor_list.remove('return_t_plus')
-
-    # 去极值
-    for column in factor_list:
-        data_3[column] = filter_EV_MdAD(data_3[column], 5)
-
-    # 中性化和序列化
-    factor_list.append('DP')
-    data_3_ind = wind_ind_dict(data_3, date=section_dict[section_num])
-    data_3_ind_LnMtkCap = pd.concat([data_3['ln_capital_4ols'], data_3_ind], axis=1, join_axes=[data_3.index])
-
-    for column in factor_list:
-        # 中性化：
-        neu_model = sm.OLS(data_3[column], data_3_ind_LnMtkCap).fit()
-        data_3[column] = neu_model.resid
-        # 序列化：
-        data_3_1 = data_3.sort_values(by=column)
-        data_3_1[column] = list(range(1, len(data_3)+1))
-        data_4[column] = data_3_1[column] / len(data_3)
-
-    # 输出文件
-    data_4.to_csv('data_4/%s_4.csv' % section_num)
 
 
 
 
-
-
-
-
+"""
 
 
